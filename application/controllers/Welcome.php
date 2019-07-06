278954a5591c862d503a9e611baceafc0780413a
Samuel lucas <samuellucas0603@gmail.com>
2019-07-06 16:30:52 -0300

    Configurando .htaccess, pacote vlucas/phpdotenv e envio de email

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,44 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('index.php');
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function index(){
+		$this->load->view('index');
+	}
+
+	public function enviar(){
+
+		$nome = $this->input->post('nome');
+		$sobrenome = $this->input->post('sobrenome');
+		$email = $this->input->post('email');
+		$mensagem = $this->input->post('mensagem');
+
+		$this->manda_email($nome,$sobrenome,$email,$mensagem);
+		$this->session->set_userdata("notice", "Sua messagem foi enviada com sucesso, assim que possivel, entraremos em contato. A Vitroart agradece sua mensagem!");
+		redirect(base_url());
+	}
+
+	public function manda_email($nome,$sobrenome,$email,$mensagem){
+		$config_email = Array(
+			'protocol' => 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_port' => 465,
+			'smtp_user' => getenv("EMAIL_SEND"),
+			'smtp_pass' => getenv("PASS_EMAIL_SEND"),
+			'mailtype'  => 'html', 
+			'charset'   => 'utf-8'
+		);
+
+		
+		$this->load->library('email', $config_email);
+		$this->email->set_newline("\r\n");
+		$this->email->from(getenv("EMAIL_CLI"));
+		$this->email->to($email);
+		$this->email->subject('Contato Site Vitroart');
+		$this->email->message($mensagem);
+		$this->email->send();
 	}
 }
